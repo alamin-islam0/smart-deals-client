3cc96a3d7764ce789a59189cb28066662af80315
Alamin Islam <alamin648890@gmail.com>
2025-11-04 22:29:59 +0600

    Login and register page added also google sign in + Email & pass sign in feature added

---
+++ src/LayOut/RootLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import NavBar from '../components/NavBar/NavBar';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <NavBar>
+                
+            </NavBar>
+            <Outlet>
+
+            </Outlet>
+        </div>
+    );
+};
+
+export default RootLayout;
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,142 @@
+import React, { use } from "react";
+import { Link } from "react-router";
+import { AuthContext } from "../../context/AuthContext";
+
+
+const Register = () => {
+
+  const {signInWithGoogle} = use(AuthContext)
+
+  const handleGoogleSignIn = () =>{
+    signInWithGoogle()
+    .then(result => {
+        console.log(result)
+    })
+    .catch(error =>{
+        console.log(error)
+    });
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-lg shadow-md">
+        <div>
+          <h2 className="text-center text-3xl font-bold text-gray-900">
+            Register Now!
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Already have an account?{" "}
+            <Link className="text-primary" to={"/login"}>
+              Login
+            </Link>
+          </p>
+        </div>
+        <form className="mt-8 space-y-6" >
+          <div className="space-y-4">
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500"
+                placeholder="Your Name"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500"
+                placeholder="your.email@example.com"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="image-url"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Image-URL
+              </label>
+              <input
+                id="image-url"
+                name="image-url"
+                type="text"
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500"
+                placeholder="http://your-image-url.com"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500"
+                placeholder="••••••••"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300"></div>
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="px-2 bg-white text-gray-500">OR</span>
+            </div>
+          </div>
+
+          <div className="mt-6">
+            <button 
+              onClick={handleGoogleSignIn}
+              type="button"
+              className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+            >
+              <img
+                className="h-5 w-5 mr-2"
+                src="https://www.svgrepo.com/show/475656/google-color.svg"
+                alt="Google logo"
+              />
+              Sign Up With Google
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
